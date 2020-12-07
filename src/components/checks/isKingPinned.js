6931d08e9430b3600d16b5eb0cd91bfd7e91f159
Kevin Desktop <kchilds2020@gmail.com>
2020-12-07 10:52:04 -0600

    make the update board method more universal

--- src/components/checks/isKingPinned.js
+++ src/components/checks/isKingPinned.js
@@ -1,35 +1,39 @@
-import findLocationFromObject from "../utilities/findLocationFromObject"
+/* import findLocationFromObject from "../utilities/findLocationFromObject"
 import findKingLocation from '../utilities/findKingLocation'
-import availableSquares from '../utilities/available-squares/availableSquares'
+import availableSquares from '../utilities/available-squares/availableSquares' */
+//import isInCheck from './isInCheck'
 
 
-const kingDangerChecks = (currentObj,desiredObj,position, setState) => {
-    let d = findLocationFromObject(desiredObj)
+const isKingPinned = (currentObj,desiredObj,position) => {
+    /* let d = findLocationFromObject(desiredObj)
+    let c = findLocationFromObject(currentObj)
     let k = currentObj.piece.slice(0,5) === 'black' ? findKingLocation(position, 'black') : findKingLocation(position, 'white');
     let temp = position;
     let desObj = desiredObj;
-    let res = true;
+    let res = false;
     desObj.piece = currentObj.piece;
     temp[d.rank][d.file] = desObj;
-    console.log(d, k, temp[d.rank][d.file])
-    availableSquares(position,setState)
+    //temp[c.rank][c.file].piece = '';
+    console.log(d, k, temp)
+    temp = availableSquares(temp)
     console.log(d, k, temp[d.rank][d.file], temp[k.rank][k.file])
     
     //find available squares for temp;
-        for(let r = 0; r < 7; r++){
+         for(let r = 0; r < 7; r++){
             for(let c = 0; c < 7; c++){
                 if(temp[r][c].avSquares){
                     temp[r][c].avSquares.forEach(element => {
                         console.log(element, k)
                         if(element.rank === k.rank && element.file === k.file){
                             console.log('IN CHECK')
-                            res = false;
+                            res = true;
                         }
                     })
                 }
+                if(res === true) temp[r][c].piece = ''
             }
-        }
-        return res;
+        } 
+        return res; */
 }
 
-export default kingDangerChecks
+export default isKingPinned
